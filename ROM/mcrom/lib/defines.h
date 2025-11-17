277bb88389bedc77850dafea92c8bdf9c31a7a0a
Machina Speculatrix <smd@h4xr.com>
2025-11-17 10:55:14 +0100

    clean up

---
+++ ROM/mcrom/lib/defines.h
@@ -0,0 +1,38 @@
+// Pin assignments
+
+// FOR DIP-BASED PROTOTYPE
+
+#define RST_PORT PORTA
+#define RST PIN4_bm
+#define RST_CTRL PIN4CTRL
+
+
+#define CTRL_PORT PORTE
+#define CTRL_PORT_INT_VEC PORTE_PORT_vect
+#define CE PIN3_bm
+#define CE_CTRL PIN3CTRL
+#define OE PIN2_bm
+#define OE_CTRL PIN2CTRL
+
+#define DATA_PORT PORTD
+#define ADDR_PORTL PORTA
+#define ADDR_PORTH PORTF
+
+
+// FOR TQFP-BASED PCB
+
+// #define RST_PORT PORTA
+// #define RST PIN4_bm
+// #define RST_CTRL PIN4CTRL
+
+
+// #define CTRL_PORT PORTE
+// #define CTRL_PORT_INT_VEC PORTE_PORT_vect
+// #define CE PIN1_bm
+// #define CE_CTR1 PIN1CTRL
+// #define OE PIN3_bm
+// #define OE_CTRL PIN3CTRL
+
+// #define DATA_PORT PORTC
+// #define ADDR_PORTL PORTA
+// #define ADDR_PORTH PORTF
